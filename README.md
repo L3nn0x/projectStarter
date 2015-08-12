4d1934b34d02cbe3329b23d5a1796908334394d8
Faerie <Faerie@test.com>
2015-08-12 17:19:59 +0100

    Added flags + ldflags and modified README

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 This is a script to rapidly start a C/C++ project.
 
-Usage : ./starter \<C/C++\> \<project_name\> [git link]
-
-If a git link is provided, a git clone will be done.
+Usage : ./starter.py -h
--- script.py
+++ script.py
@@ -4,42 +4,51 @@ import argparse
 from subprocess import call
 import os
 
+def getPathName(pathname):
+    tmp = os.path.split(pathname)
+    return tmp[-1]
+
 parser = argparse.ArgumentParser(description='Create and setup a C/C++ project')
 parser.add_argument('C/C++', choices=['C', 'C++'])
 parser.add_argument('project_name')
-parser.add_argument('-l', '--link', required=False)
-parser.add_argument('-f', '--flags', required=False, default='-Wall -Werror')
+parser.add_argument('-f', '--flags', required=False)
+parser.add_argument('-l', '--ldflags', required=False)
 args = parser.parse_args()
 args = vars(args)
 
-name = args['project_name']
-if os.path.exists(name) and os.path.isdir(name):
+pathname = args['project_name']
+name = getPathName(pathname)
+
+if os.path.exists(pathname) == True or os.path.isdir(pathname) == True:
     print("The folder already exists!\nExiting...")
     exit(1)
 
+ldflags = args['ldflags']
 flags = args['flags']
-
-if args['link']:
-    call(["git", "clone", args['link'], args['project_name']])
+if not flags:
+    flags = '-Wall -Werror'
+    if args['C/C++'] == 'C++':
+        flags += ' -std=c++11'
 
 dirs = ('src', 'obj', 'bin', 'include', 'lib', 'depends')
 
+print("Creating folders...")
 for i in dirs:
-    call(["mkdir", "-p", '/'.join((name, i))])
+    os.makedirs(os.path.join(pathname, i))
+print("Done!")
 
 readme = """This is a sample readme for %s.
 You should change it as soon as possible!""" % name
-if args['link']:
-    readme += "It is linked to %s" % args['link']
 
-call("printf \"" + readme + "\" > " + '/'.join((name, 'README.md')), shell=True)
+print("Writing README...")
+with open(os.path.join(pathname, 'README.md'), 'w') as f:
+    f.write(readme)
+print("Done!")
 
 cc = 'gcc'
-if args['C/C++'] == 'C++':
-    cc = 'g++'
-
 ext = 'c'
 if args['C/C++'] == 'C++':
+    cc = 'g++'
     ext = 'cpp'
 
 common = """TOP := $(dir $(lastword $(MAKEFILE_LIST)))
@@ -47,6 +56,7 @@ SRC_DIR := $(TOP)src/
 INCLUDE_DIR := $(TOP)include/
 BIN_DIR := $(TOP)bin/
 OBJ_DIR := $(TOP)obj/
+LIB_DIR := $(TOP)lib/
 DEPENDS_DIR := $(TOP)depends/
 
 CC := {0}
@@ -58,14 +68,14 @@ else
 \tCFLAGS += -O2
 endif
 CPPFLAGS := -I$(INCLUDE_DIR)
-LDFLAGS :=
+LDFLAGS := -L$(LIB_DIR) {3}
 
 $(OBJ_DIR)%.o: $(SRC_DIR)%.{2}
 \t$(CC) $(CPPFLAGS) $(CFLAGS) $< -o $@
 
 $(DEPENDS_DIR)%.d: $(SRC_DIR)%.{2}
 \t$(CC) $(CPPFLAGS) $(CFLAGS) -MM $(<:$(SRC_DIR).{2}=.o) $^ > $@
-""".format(cc, flags, ext)
+""".format(cc, flags, ext, ldflags)
 
 makefile = """include Makefile.common
 
@@ -93,15 +103,46 @@ mrproper: clean
 include $(subst $(SRC_DIR),$(DEPENDS_DIR),$(SRC_FILES:%.{1}=%.d))
 """.format(name, ext)
 
-call("echo \'" + common + "\' > " + '/'.join((name, 'Makefile.common')), shell=True)
-call("echo \'" + makefile + "\' > " + '/'.join((name, 'Makefile')), shell=True)
+print("Writing Makefile.common...")
+with open(os.path.join(pathname, 'Makefile.common'), 'w') as f:
+    f.write(common)
+print("Done!")
+print("Writing Makefile...")
+with open(os.path.join(pathname, 'Makefile'), 'w') as f:
+    f.write(makefile)
+print("Done!")
 
 hello = """#include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    printf("Hello world!\\n");
     return 0;
 }
 """
-call("echo \'" + hello + "\' > " + '/'.join((name, 'src', 'main.' + ext)), shell=True)
-print("Done")
+
+print("Writing default src file...")
+with open(os.path.join(os.path.join(pathname, 'src'), 'main.' + ext), 'w') as f:
+    f.write(hello)
+print("Done!")
+
+gitignore = """%s
+*.o
+*.d
+*~
+#*
+*.*.sw*
+""" % name
+print("Adding .gitignore to every folders...")
+for i in dirs:
+    with open(os.path.join(os.path.join(pathname, i), '.gitignore'), 'w') as f:
+        f.write(gitignore)
+
+print("Initializing git repository and creating first commit")
+w = os.getcwd()
+os.chdir(pathname)
+call(['git', 'init'])
+call(['git', 'add', '.'])
+call(['git', 'commit', '-m', 'First commit'])
+os.chdir(w)
+print("Done!")
+print("\n\nWorking folder configured!")
